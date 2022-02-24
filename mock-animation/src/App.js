ac1a2837686b781616c09fd165b9d7c3a92a0305
Jude Harvey Maldicas <jude.maldicas@betterask.erni>
2022-02-24 13:35:36 +0800

    Update package, add initial routes

--- mock-animation/src/App.js
+++ mock-animation/src/App.js
@@ -1,10 +1,19 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
 import Home from './components/Home.js';
+import About from './components/About.js';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Route path="/" component={ Home } exact />
+        <Route path="/about" component={ About } />
+      </div>
+    </Router>
   );
 }
 
